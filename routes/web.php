4b0c616de381e9615bad322eaa0a02c948708e5d
REGINALDO SOUZA <reginaldo.aparecido.souzas@gmail.com>
2026-09-11 19:32:03 -0300

    Remove rota duplicada do PWA

--- routes/web.php
+++ routes/web.php
@@ -421,17 +421,7 @@ Route::middleware('auth')->group(function () {
         )->name('assinaturas.cancelar');
     });
 
-    Route::get('/app', function () {
-
-        if (auth()->check()) {
-            return redirect()->route('dashboard');
-        }
-
-        return redirect()->away(
-            'https://sgasistema.com.br/abrir-sistema'
-        );
-
-    })->name('app.inicio');
+    
 
 
 });
